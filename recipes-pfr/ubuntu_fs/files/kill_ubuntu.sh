489f21ec130d993816bab029b35e4485bdb74e87
UEFI-code <zhuoshizhang@hotmail.com>
2026-06-29 15:15:36 +0900

    Improve kill script

--- recipes-pfr/ubuntu_fs/files/kill_ubuntu.sh
+++ recipes-pfr/ubuntu_fs/files/kill_ubuntu.sh
@@ -1,7 +1,7 @@
 for p in /proc/[0-9]*; do
     pid=${p##*/}
     root=$(readlink $p/root 2>/dev/null)
-    if [ "$root" != "/" ]; then
+    if [[ "$root" == *"/mnt/ubuntu24"* ]]; then
         echo "$pid $root"
 	kill -9 $pid
     fi
